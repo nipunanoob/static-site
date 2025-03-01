c082939146401ce4e4718cef98aa0b113982a0f6
nipunanoob <nipunanoob@gmail.com>
2025-03-01 15:19:23 +0530

    Add functionality to split text node based on delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -71,6 +71,106 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a image node", "test")
         with self.assertRaisesRegex(ValueError, f"Invalid TextType: {node.text_type}"):
             html_node = text_node_to_html_node(node)
+    
+    def test_split_nodes_code_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("This is text with a ", TextType.TEXT),
+                        TextNode("code block", TextType.CODE),
+                        TextNode(" word", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_nodes_bold_delimiter(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expected = [
+                        TextNode("This is text with a ", TextType.TEXT),
+                        TextNode("bold block", TextType.BOLD),
+                        TextNode(" word", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_nodes_italic_delimiter(self):
+        node = TextNode("This is text with a _italic block_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        expected = [
+                        TextNode("This is text with a ", TextType.TEXT),
+                        TextNode("italic block", TextType.ITALIC),
+                        TextNode(" word", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_nodes_missing_closing_delimiter(self):
+        node = TextNode("This is text with missing `clossing delimiter", TextType.TEXT)
+        with self.assertRaisesRegex(Exception, "Delimiter ` was not closed!"):
+            split_nodes_delimiter([node], "`",TextType.CODE)
+
+    def test_split_nodes_no_delimiter(self):
+        node = TextNode("This is text with no Delimiter", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        expected = [
+                        TextNode("This is text with no Delimiter", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_non_text_node(self):
+        node = TextNode("This is a code block", TextType.CODE)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("This is a code block", TextType.CODE),
+                    ]
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_beginning_delimiter(self):
+        node = TextNode("`test` outside block", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("test", TextType.CODE),
+                        TextNode(" outside block", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_ending_delimiter(self):
+        node = TextNode("outside block `test`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("outside block ", TextType.TEXT),
+                        TextNode("test", TextType.CODE),
+                    ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_multiple_delimiter(self):
+        node = TextNode("This is `block 1` and `block 2`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("This is ", TextType.TEXT),
+                        TextNode("block 1", TextType.CODE),
+                        TextNode(" and ", TextType.TEXT),
+                        TextNode("block 2", TextType.CODE),
+                    ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_non_text_and_text_node(self):
+        nodes = [TextNode("This is a code block", TextType.CODE),TextNode("This is a `code block` bro", TextType.TEXT)] 
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        expected = [
+                        TextNode("This is a code block", TextType.CODE),
+                        TextNode("This is a ", TextType.TEXT),
+                        TextNode("code block", TextType.CODE),
+                        TextNode(" bro", TextType.TEXT),
+                    ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_consecutive_delimiter(self):
+        node = TextNode("`code1``code2`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                        TextNode("code1", TextType.CODE),
+                        TextNode("code2", TextType.CODE),
+                    ]
+        self.assertEqual(new_nodes, expected)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -42,3 +42,21 @@ def text_node_to_html_node(text_node: TextNode):
         return LeafNode("img", "" , {"src": text_node.url, "alt": text_node.text})
     else:
         raise ValueError(f"Invalid TextType: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        node_text_split = node.text.strip().split(delimiter)
+        if len(node_text_split) % 2 == 0:
+            raise Exception(f"Delimiter {delimiter} was not closed!")
+        else:
+            for index in range(len(node_text_split)):
+                if index % 2 == 0:
+                    if node_text_split[index] != "":
+                        new_nodes.append(TextNode(node_text_split[index], TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(node_text_split[index], text_type))
+    return new_nodes
